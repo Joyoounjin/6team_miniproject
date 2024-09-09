0359cc17d1fec01546fafe5350d7a659f49f85e0
BL4CK DoG <xoxohoon01@gmail.com>
2024-09-09 14:18:47 +0900

    Init

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager Instance;
+
+    public AudioSource audioSource;
+    public AudioClip clip;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        
+        audioSource.clip = this.clip;
+        audioSource.Play();
+
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,92 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+
+    public Text timeTxt;
+    float time = 0.0f;
+
+    public Card firstCard;
+    public Card secondCard;
+
+    public int cardCount = 0;
+    public GameObject endTxt;
+
+    public AudioClip clip;
+    public AudioSource audioSource;
+
+
+    public void isMatched()
+    {
+        if (firstCard.index == secondCard.index)
+        {
+            audioSource.PlayOneShot(clip);
+
+            firstCard.DestroyCard();
+            secondCard.DestroyCard();
+            cardCount -= 2;
+
+            if (cardCount == 0)
+            {
+                endTxt.SetActive(true);
+                Time.timeScale = 0.0f;
+            }
+        }
+        else
+        {
+            firstCard.CloseCard();
+            secondCard.CloseCard();
+        }
+
+        firstCard = null;
+        secondCard = null;
+    }
+
+    public void checkMatched()
+    {
+        if (firstCard.index == secondCard.index)
+        {
+            firstCard.DestroyCard();
+            secondCard.DestroyCard();
+        }
+        else
+        {
+            firstCard.CloseCard();
+            secondCard.CloseCard();
+        }
+
+        firstCard = null;
+        secondCard = null;
+    }
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+    }
+
+    void Start()
+    {
+        Time.timeScale = 1.0f;
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    private void Update()
+    {
+        time += Time.deltaTime;
+        timeTxt.text = time.ToString("N2");
+        if (time >= 30.0f)
+        {
+            endTxt.SetActive(true);
+            Time.timeScale = 0.0f;
+        }
+
+    }
+}
